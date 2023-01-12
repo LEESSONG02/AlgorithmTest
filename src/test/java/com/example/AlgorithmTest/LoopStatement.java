16182726d5ea6c3502eb23fc70f1382a800ae951
LEESSONG02 <leessong@kakao.com>
2023-01-12 10:52:30 +0900

    230112_문제 1,2 업로드

--- src/test/java/com/example/AlgorithmTest/LoopStatement.java
+++ src/test/java/com/example/AlgorithmTest/LoopStatement.java
@@ -1,7 +1,44 @@
 package com.example.AlgorithmTest;
 
+import java.util.Arrays;
+
 public class LoopStatement {
 
 
+	public String printMultiplicationTable(int num) {
+		if(!(1 <= num && num <= 9))
+			throw new IllegalArgumentException();
 
+		StringBuffer sb = new StringBuffer();
+
+		for (int i = 1; i <= 9; i++) {
+			sb.append(String.format("%d * %d = %d", num, i, num*i));
+			if(i != 9)
+				sb.append("\r\n");
+		}
+
+		return sb.toString();
+	}
+
+	public String printPlus(String caseStr) {
+		String[] caseStrArr = caseStr.split("\r\n");
+		int caseCnt = Integer.parseInt(caseStrArr[0]);
+		if(caseCnt != caseStrArr.length -1)
+			throw new IllegalArgumentException();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 1; i < caseStrArr.length; i++) {
+			String[] eachCaseArr = caseStrArr[i].split(" ");
+			int[] eachCaseIntArr = Arrays.stream(eachCaseArr).mapToInt(Integer::parseInt).toArray();
+			int sum = 0;
+			for (int i1 : eachCaseIntArr) {
+				if(!(0 < i1 && i1 < 10))
+					throw new IllegalArgumentException();
+				sum += i1;
+			}
+			sb.append(sum);
+			if(i != caseStrArr.length -1)
+				sb.append("\r\n");
+		}
+		return sb.toString();
+	}
 }
--- src/test/java/com/example/AlgorithmTest/LoopStatementTest.java
+++ src/test/java/com/example/AlgorithmTest/LoopStatementTest.java
@@ -1,18 +1,78 @@
 package com.example.AlgorithmTest;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LoopStatementTest {
+
+	private static LoopStatement ls;
+	@BeforeAll
+	static void init(){
+		ls = new LoopStatement();
+	}
+
+
 	@DisplayName("구구단 출력 프로그램")
 	@Test
-	public void multiplicationTableTest(){
+	public void printMultiplicationTableTest(){
+		Assertions.assertThatIllegalArgumentException().isThrownBy(
+			() -> ls.printMultiplicationTable(200)
+		);
+		String result = ls.printMultiplicationTable(2);
+		Assertions.assertThat(result).isEqualTo("2 * 1 = 2\r\n" +
+				"2 * 2 = 4\r\n" +
+				"2 * 3 = 6\r\n" +
+				"2 * 4 = 8\r\n" +
+				"2 * 5 = 10\r\n" +
+				"2 * 6 = 12\r\n" +
+				"2 * 7 = 14\r\n" +
+				"2 * 8 = 16\r\n" +
+				"2 * 9 = 18"
+		);
+		String result2 = ls.printMultiplicationTable(9);
+		Assertions.assertThat(result2).isEqualTo("9 * 1 = 9\r\n" +
+				"9 * 2 = 18\r\n" +
+				"9 * 3 = 27\r\n" +
+				"9 * 4 = 36\r\n" +
+				"9 * 5 = 45\r\n" +
+				"9 * 6 = 54\r\n" +
+				"9 * 7 = 63\r\n" +
+				"9 * 8 = 72\r\n" +
+				"9 * 9 = 81");
 
 	}
-
-	@DisplayName("")
+	@DisplayName("두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램")
+	@Test
+	public void printPlusTest(){
+		Assertions.assertThatIllegalArgumentException().isThrownBy(
+			() -> ls.printPlus("5\r\n" +
+					"10 1\r\n" +
+					"2 3\r\n" +
+					"3 4\r\n" +
+					"9 8\r\n" +
+					"5 2")
+		);
+		String result = ls.printPlus("5\r\n" +
+				"1 1\r\n" +
+				"2 3\r\n" +
+				"3 4\r\n" +
+				"9 8\r\n" +
+				"5 2"
+		);
+		Assertions.assertThat(result).isEqualTo("2\r\n" +
+				"5\r\n" +
+				"7\r\n" +
+				"17\r\n" +
+				"7");
+	}
+	
+	@DisplayName("n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램")
 	@Test
 	public void Test(){
-
+		
 	}
+
+
 }
